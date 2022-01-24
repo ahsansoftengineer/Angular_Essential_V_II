3e43ff2bee3eadd0b9c41098507a074e9fc96e55
DAWATEISLAMI\KFMMA1597 <it.dev57@dawateislami.net>
2022-01-24 14:28:53 +0500

    SERVICES

--- state.service.ts
+++ state.service.ts
@@ -1,22 +1,9 @@
 import { Injectable, Injector } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Hierarchy } from '../interface/common/hierarchy';
-import { MonthlyTarget } from '../interface/common/monthly-target';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StateService {
-  public local_user: string;
-  public multiHierarchy = false;
-  // TRANSACTION
-  public hierarchy: Hierarchy;
-  dontShowPopup = false;
-  transBatch: boolean = true;
-  monthlyTarget: MonthlyTarget;
-  userId: string;
-  // For Ngx Loading Spinner
-  public isLoading = new BehaviorSubject(false);
   constructor(public injector: Injector) {
     // Service Injection
   }
